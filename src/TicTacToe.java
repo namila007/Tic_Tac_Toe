b9f3f57f85b664064ecd4e9602ad192e798b6a13
Namila Bandara <namila007@gmail.com>
2017-08-30 21:12:12 +0530

    changed main

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,10 +9,10 @@ public class TicTacToe {
     public static void main(String[] args) {
         TicView ticView =new TicView();
         TicModel model=new TicModel();
+
         TicController controller=new TicController(model, ticView);
 
         ticView.setTitle("Tic Toc Toe");
-        ticView.addButtons(controller);
         ticView.setSize(300,300);
         ticView.setLocationRelativeTo(null);
         ticView.setVisible(true);
